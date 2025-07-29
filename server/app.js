6014986b2da399a16f26ccb665f69f562af20787
Agustin Mole <amole@godoycruz.gov.ar>
2025-07-29 14:07:44 -0300

    cambiando las varaibles de entorno para produccion o localhost

--- server/app.js
+++ server/app.js
@@ -7,12 +7,17 @@ import ticketRoutes from "./routes/ticketRoutes.js"
 import cors from "cors"
 import dotenv from "dotenv"
 import './lib/cron.js'
+import path from 'path';  //agregado el 29/07
+import { fileURLToPath } from 'url';  //agregado el 29/07
+// Necesario para obtener el __dirname en ES Module
 
 dotenv.config()
 const app = express()
 connecDb()
 
 //setting
+const __filename = fileURLToPath(import.meta.url);  //agregado el 29/07
+const __dirname = path.dirname(__filename);  //agregado el 29/07
 
 //middleware
 app.use(express.text())
@@ -21,6 +26,16 @@ app.use(urlencoded({extended:true}))
 app.use(morgan("tiny"))
 app.use(cors())
 //routes
+// Rutas de tu API
+app.use('/api', require('./routes/api'));  //agregado el 29/07
+
+// Servir archivos estáticos de Vite
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')));  //agregado el 29/07
+
+// Ruta catch-all para manejar rutas del frontend (React)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));  //agregado el 29/07
+});
 app.use(userRoutes)
 app.use(ticketRoutes)
 
