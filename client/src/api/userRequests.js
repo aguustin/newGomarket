8b071e069b7a8aae92d34929585388a46ffe4eb9
Agustin Mole <amole@godoycruz.gov.ar>
2025-07-30 10:20:00 -0300

    cambiando las varaibles de entorno para produccion o localhost

--- client/src/api/userRequests.js
+++ client/src/api/userRequests.js
@@ -13,14 +13,14 @@ export const confirmNewPassRequest = ({token, nuevaContrasenia}) => axios.post('
 export const contactarRequest = (mailData) => axios.post('http://localhost:4000/contactar', mailData)*/
 
 
-export const getAllUsers = () => axios.get(`${process.env.VITE_URL}/getAllUsers`)
+export const getAllUsers = () => axios.get(`${import.meta.env.VITE_URL}/getAllUsers`)
 
-export const registerUserRequest = (userData) => axios.post(`${process.env.VITE_URL}/register`, userData)
+export const registerUserRequest = (userData) => axios.post(`${import.meta.env.VITE_URL}/register`, userData)
 
-export const loginUserRequest = (userData) => axios.post(`${process.env.VITE_URL}/login`, userData)
+export const loginUserRequest = (userData) => axios.post(`${import.meta.env.VITE_URL}/login`, userData)
 
-export const recoverPassRequest = ({mail}) => axios.post(`${process.env.VITE_URL}/recover_password`, {mail})
+export const recoverPassRequest = ({mail}) => axios.post(`${import.meta.env.VITE_URL}/recover_password`, {mail})
 
-export const confirmNewPassRequest = ({token, nuevaContrasenia}) => axios.post(`${process.env.VITE_URL}/new_password`, {token, nuevaContrasenia})
+export const confirmNewPassRequest = ({token, nuevaContrasenia}) => axios.post(`${import.meta.env.VITE_URL}/new_password`, {token, nuevaContrasenia})
 
-export const contactarRequest = (mailData) => axios.post(`${process.env.VITE_URL}/contactar`, mailData)
+export const contactarRequest = (mailData) => axios.post(`${import.meta.env.VITE_URL}/contactar`, mailData)
