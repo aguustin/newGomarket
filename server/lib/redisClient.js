1eb3b25e9290c3b7dbf52745e7c39a1aab661871
aguustin <agustin.molee@gmail.com>
2025-10-04 12:37:31 -0300

    intentando arreglar el pago de mercadopago

--- server/lib/redisClient.js
+++ server/lib/redisClient.js
@@ -1,13 +1,11 @@
-import { createClient } from "redis";
+import Redis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const redisUrl = process.env.REDIS_URL;  //si no conecta sacarle una 's' a "rediss" en REDIS_URL del archivo .env
-export const redisClient = createClient({ url: redisUrl });
+export const redisClient = new Redis(process.env.REDIS_URL);
 
+redisClient.on("connect", () => console.log("Redis connected successfully!"));
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
-await redisClient.connect();
-await redisClient.set('foo','bar');
-
-console.log("Redis connected successfully!");
+// Opcional: probar que funciona
+redisClient.set('foo', 'bar').catch(console.error);
