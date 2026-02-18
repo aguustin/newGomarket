d69715df63485f7438bcdb18c8bcba77f081639f
Agustin Mole <amole@godoycruz.gov.ar>
2026-02-18 08:45:30 -0300

    .

--- client/src/components/buyTicket/buyTicket.jsx
+++ client/src/components/buyTicket/buyTicket.jsx
@@ -267,7 +267,7 @@ const activeDiscountCode = async (e) => {
                             <div key={tck._id} className="flex justify-center mx-auto text-center">
                               <div className="w-full mb-3 p-4 bg-gray-800  border-2 border-gray-600 rounded-2xl hover:shadow-lg transition-all duration-300">
 
-                                <div className="flex items-center justify-between flex-wrap gap-4">
+                                <div className="flex items-center justify-between max-[550px]:justify-center flex-wrap gap-4">
 
                                   {/* Imagen + nombre + descripción */}
                                   <div className="flex items-center space-x-4 min-w-[200px]">
@@ -286,7 +286,7 @@ const activeDiscountCode = async (e) => {
                                   </div>
 
                                   {/* Precio + fecha */}
-                                  <div className="flex items-center space-x-6">
+                                  <div className="flex items-center space-x-6 max-[550px]:w-full max-[550px]:justify-center">
                                     <div className="text-center">
                                       <p className="text-xs text-gray-300 font-medium">Precio</p>
                                       <p className="text-sm text-gray-200">${tck.precio} c/u</p>
@@ -302,7 +302,7 @@ const activeDiscountCode = async (e) => {
 
                                   {/* Cantidades */}
                                   {/*tck.cantidad >= 1 && */ tck.estado !== 4 && !eventToRender?.soldOut ? (
-                                    <div className="flex items-center space-x-2 bg-gray-700 rounded-xl p-1 border border-gray-200 shadow-sm">
+                                    <div className="flex items-center space-x-2 bg-gray-700 rounded-xl p-1 border border-gray-200 shadow-sm max-[550px]:w-[90%] max-[550px]:justify-between max-[550px]:mt-2">
                                       <button
                                         className="w-8 h-8 bg-gray-100 hover:bg-gray-200 rounded-lg text-gray-700 transition-all active:scale-95" 
                                         onClick={(e) => restQuantity(e, tck._id, tck.limit)}
@@ -332,7 +332,7 @@ const activeDiscountCode = async (e) => {
                                     </div>
                                   )}
                                   {tck.cantidad >= 1 && eventToRender?.soldOut === false && (
-                                    <p className="text-gray-800 font-medium text-sm mt-2">
+                                    <p className="text-white-800 font-medium text-sm mt-2">
                                       {currencyFormatter.format((quantities[tck._id]?.amount || 0) * tck.precio)}
                                     </p>
                                   )}
@@ -370,10 +370,10 @@ const activeDiscountCode = async (e) => {
                         <input className="w-[100%] text-gray-200!" type="number" name="telefono" placeholder="..."></input>
                     </div>
                 </div>
-                <div className="flex items-center mt-4">
+                <div className="flex flex-wrap justify-center items-center mt-4">
                   <p>Codigo de descuento:</p>
                   <input className="border-[1px] border-gray-400 rounded-lg p-2 ml-1 text-gray-300!" type="text" onChange={(e) => setDiscountValueCode(e.target.value)}></input>
-                  <button className="flex items-center py-2 px-3 ml-2 bg-gradient-to-r from-amber-500 to-yellow-500 rounded-lg" type="button" onClick={(e) => activeDiscountCode(e)}>
+                  <button className="flex items-center py-2 px-3 ml-2 bg-gradient-to-r from-amber-500 to-yellow-500 rounded-lg max-[630px]:mt-3" type="button" onClick={(e) => activeDiscountCode(e)}>
                     <svg viewBox="0 0 24 24" width={24} className="mr-1" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3.9889 14.6604L2.46891 13.1404C1.84891 12.5204 1.84891 11.5004 2.46891 10.8804L3.9889 9.36039C4.2489 9.10039 4.4589 8.59038 4.4589 8.23038V6.08036C4.4589 5.20036 5.1789 4.48038 6.0589 4.48038H8.2089C8.5689 4.48038 9.0789 4.27041 9.3389 4.01041L10.8589 2.49039C11.4789 1.87039 12.4989 1.87039 13.1189 2.49039L14.6389 4.01041C14.8989 4.27041 15.4089 4.48038 15.7689 4.48038H17.9189C18.7989 4.48038 19.5189 5.20036 19.5189 6.08036V8.23038C19.5189 8.59038 19.7289 9.10039 19.9889 9.36039L21.5089 10.8804C22.1289 11.5004 22.1289 12.5204 21.5089 13.1404L19.9889 14.6604C19.7289 14.9204 19.5189 15.4304 19.5189 15.7904V17.9403C19.5189 18.8203 18.7989 19.5404 17.9189 19.5404H15.7689C15.4089 19.5404 14.8989 19.7504 14.6389 20.0104L13.1189 21.5304C12.4989 22.1504 11.4789 22.1504 10.8589 21.5304L9.3389 20.0104C9.0789 19.7504 8.5689 19.5404 8.2089 19.5404H6.0589C5.1789 19.5404 4.4589 18.8203 4.4589 17.9403V15.7904C4.4589 15.4204 4.2489 14.9104 3.9889 14.6604Z" stroke="#111827" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 15L15 9" stroke="#111827" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14.4945 14.5H14.5035" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9.49451 9.5H9.50349" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                     <p className="text-[#111827]">Aplicar descuento</p>
                   </button>
